4a0969e9aa5e9dab9206d8064bf41e2f244a92a7
nicoackerman <nicolascardenas0115@gmail.com>
2025-09-08 14:40:39 +0000

    added about me data

---
+++ src/data/aboutMe.tsx
@@ -0,0 +1,13 @@
+const _me = {
+  Experience: {
+    amount: "+1",
+    label: "YEARS OF EXPERIENCE",
+  },
+  Projects: {
+    amount: "+3",
+    label: "PROJECTS COMPLETED",
+  },
+};
+
+const ABOUT_ME = Object.entries(_me);
+export default ABOUT_ME;
